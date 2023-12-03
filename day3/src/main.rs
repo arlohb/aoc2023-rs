556f76758e7016e9543a8b82d6f679a9045f5511
arlohb <arlo@urwick.co.uk>
2023-12-03 13:34:49 +0000

    Day 3 part 1

---
+++ day3/src/main.rs
@@ -0,0 +1,63 @@
+#![warn(clippy::unwrap_used, clippy::pedantic, clippy::nursery)]
+#![allow(
+    clippy::module_inception,
+    clippy::module_name_repetitions,
+    clippy::cast_precision_loss,
+    clippy::cast_possible_truncation,
+    clippy::cast_sign_loss
+)]
+
+use anyhow::Context;
+
+fn next_to_symbol(
+    lines: &[&str],
+    n_start: usize,
+    n_end: usize,
+    line_i: usize,
+) -> anyhow::Result<bool> {
+    for line in &lines[line_i.saturating_sub(1)..=(line_i + 1).min(lines.len() - 1)] {
+        for scan_ci in n_start.saturating_sub(1)..=(n_end + 1).min(line.len() - 1) {
+            let c = line.chars().nth(scan_ci).context("Invalid input")?;
+
+            if !c.is_ascii_digit() && c != '.' {
+                return Ok(true);
+            }
+        }
+    }
+
+    Ok(false)
+}
+
+const PART1: bool = false;
+
+fn main() -> anyhow::Result<()> {
+    let input = std::fs::read_to_string("input.txt")?;
+    let lines = input.lines().collect::<Vec<_>>();
+
+    let mut sum = 0;
+
+    if PART1 {
+        for (line_i, line) in lines.iter().enumerate() {
+            let mut n_pair: Option<(u32, usize)> = None;
+
+            for (c_i, c) in line.chars().chain(std::iter::once('.')).enumerate() {
+                if let Some(digit) = c.to_digit(10) {
+                    if let Some((n, _)) = &mut n_pair {
+                        *n = 10 * *n + digit;
+                    } else {
+                        n_pair = Some((digit, c_i));
+                    }
+                } else if let Some((n, n_start)) = n_pair {
+                    if next_to_symbol(&lines, n_start, c_i - 1, line_i)? {
+                        sum += n;
+                    };
+                    n_pair = None;
+                }
+            }
+        }
+    }
+
+    println!("{sum}");
+
+    Ok(())
+}
