be84ae34d5127835626cf4902643cb91dfe8aff9
arlohb <arlo@urwick.co.uk>
2023-12-07 17:25:29 +0000

    Day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -11,42 +11,66 @@ use std::str::FromStr;
 
 use anyhow::Context;
 
-const fn dist_travelled(race_time: u32, btn_time: u32) -> u32 {
+const fn dist_travelled(race_time: u64, btn_time: u64) -> u64 {
     let travel_time = race_time - btn_time;
     let speed = btn_time;
     travel_time * speed
 }
 
-fn num_of_wins(race_time: u32, dist: u32) -> u32 {
+fn num_of_wins(race_time: u64, dist: u64) -> u64 {
     (0..race_time)
         .filter(|btn_time| dist_travelled(race_time, *btn_time) > dist)
-        .count() as u32
+        .count() as u64
 }
+
+const PART1: bool = false;
 
 fn main() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
     let (times_str, dists_str) = input.split_once('\n').context("Invalid input")?;
 
-    let times = times_str
-        .split_whitespace()
-        .skip(1)
-        .map(u32::from_str)
-        .collect::<Result<Vec<_>, _>>()?;
+    if PART1 {
+        let times = times_str
+            .split_whitespace()
+            .skip(1)
+            .map(u64::from_str)
+            .collect::<Result<Vec<_>, _>>()?;
+        let dists = dists_str
+            .split_whitespace()
+            .skip(1)
+            .map(u64::from_str)
+            .collect::<Result<Vec<_>, _>>()?;
 
-    let dists = dists_str
-        .split_whitespace()
-        .skip(1)
-        .map(u32::from_str)
-        .collect::<Result<Vec<_>, _>>()?;
+        let count = times
+            .into_iter()
+            .zip(dists)
+            .map(|(race_time, dist)| num_of_wins(race_time, dist))
+            .reduce(|acc, n| acc * n)
+            .context("No wins found")?;
 
-    let count = times
-        .into_iter()
-        .zip(dists)
-        .map(|(race_time, dist)| num_of_wins(race_time, dist))
-        .reduce(|acc, n| acc * n)
-        .context("No wins found")?;
+        println!("{count}");
+    } else {
+        let time = times_str
+            .split_once(':')
+            .context("Invalid input")?
+            .1
+            .chars()
+            .filter(|c| !c.is_whitespace())
+            .collect::<String>()
+            .parse::<u64>()?;
+        let dist = dists_str
+            .split_once(':')
+            .context("Invalid input")?
+            .1
+            .chars()
+            .filter(|c| !c.is_whitespace())
+            .collect::<String>()
+            .parse::<u64>()?;
 
-    println!("{count}");
+        let count = num_of_wins(time, dist);
+
+        println!("{count}");
+    };
 
     Ok(())
 }
